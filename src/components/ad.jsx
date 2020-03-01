a2aa01c92120220af81bb34873095974bf1e73e0
Karen Borrero <keba2503@gmail.com>
2020-03-01 21:12:53 +0000

    filter

--- src/components/ad.jsx
+++ src/components/ad.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import { adApi, tags } from '../services/api';
-import Adcard from './AdCard';
+import { Link } from "react-router-dom";
 import '../App.css';
-import Adfilter  from './filter'
 
+const TAGS = [
+  {
+    id: "id_motor",
+    name: "Motor"
+  },
+  {
+    id: "id_work",
+    name: "Work"
+  },
+  {
+    id: "id_lifestyle",
+    name: "Lifestyle"
+  },
+  {
+    id: "id_mobile",
+    name: "Mobile"
+  }
+];
 
 export default class Ads extends Component {
     constructor(props) {
@@ -11,6 +28,7 @@ export default class Ads extends Component {
       this.state = {
         ads: '',
         tags: '',
+        tagsAd: TAGS[0].id
       };
     }
   
@@ -28,14 +46,82 @@ export default class Ads extends Component {
     }
   }
 
+ adChange = event => {
+    this.setState({ tagsAd: event.target.value });
+  };
+  
+
   render() {
-    const { ads } = this.state
+  
+    const {tagsAd, ads } = this.state;
     if (ads !== '') {
 
       return (
-        <div>         
-          <Adfilter></Adfilter>
-          <Adcard data={ads}></Adcard>
+        <div> 
+
+          <div className='wrapper-2'>
+            <div className="container-2">
+              <h1>Walla-Keb</h1>
+              <br></br>
+              <h2>Search Ads</h2>
+              <form className='form' onSubmit={this.Submit}>
+                <div>
+                  <label name="username">Ads</label>
+                  <input name="username" type="text" onChange={this.Input} />
+                </div>
+                <div>
+                  <link rel="stylesheet" href="" />
+                  <button className='login-button'>Search</button>
+                  <br />
+                  <label className='login-button' htmlFor="">Tags:</label>
+                  <select className='login-button' value={tagsAd} onChange={this.adChange}>
+                    {TAGS.map(tagsAd => (
+                      <option value={tagsAd.id}>{tagsAd.name}</option>
+                    ))}
+                  </select>
+                </div>
+              </form>
+
+            </div>
+        <ul className="bg-bubbles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>        
+      <div className="cards">
+      {ads.map((ad) => {
+    return (    
+  <li key className="cards__item">
+    <div className="card">
+      <div className="card__image"> <img src={ad.photo} alt="AdImage" /></div>
+      <div className="card__content">
+        <div className="card__title"><h2>{ad.name}</h2></div>
+        <p className="card__text"> <h2>Descriptión:</h2> {ad.description}  <hr/>
+        <p><h3>Precio:{ad.price} </h3> </p>
+          <p><h3>Tipo:  {ad.type}</h3> </p>
+        Perspiciatis doloribus distinctio, ducimus at labore voluptatum iure impedit.
+         Officia ratione corrupti facere porro eaque suscipit, iure esse in, sit dolore doloribus!  
+        
+         </p>
+         <Link to={`/detail/${ad._id}`}>
+        <button className="btn btn--block card__btn">Detail</button>
+        </Link>
+      </div>
+    </div>
+  </li>   
+ 
+    );
+  })
+  }
+     </div>   
         </div>
       );
     } else {
